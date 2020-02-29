4468074464a411459388f29882d116cde56fd35e
Edgar Noel Salmerón Ramírez <12400976+edgarnsr@users.noreply.github.com>
2020-02-29 11:29:16 -0700

    Update GeoNotificationNotifier.java

--- src/android/GeoNotificationNotifier.java
+++ src/android/GeoNotificationNotifier.java
@@ -9,8 +9,8 @@ import android.graphics.BitmapFactory;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.TaskStackBuilder;
 import android.util.Log;
 
 import java.io.IOException;
